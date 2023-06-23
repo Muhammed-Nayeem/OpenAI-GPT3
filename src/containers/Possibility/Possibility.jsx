a557098383d4db508bd8e6dd2b5cc2cae96735ba
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-06-23 19:05:27 +0600

    Possibility Component Markup, Style and Responsiveness has done

--- src/containers/Possibility/Possibility.jsx
+++ src/containers/Possibility/Possibility.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import "./Possibility.css";
+import app from "../../assets/possibility.png"
 
 const Possibility = () => {
   return (
-    <div>
-      <h3>Possibility</h3>
-    </div>
+    <section className="gpt3__possibility" id="possibility">
+      <div className="container">
+        <div className="gpt3__possibility-content">
+          <div className="gpt3__possibility-img">
+            <img src={app} alt="Possibility" />
+          </div>
+          <div className="gpt3__possibility-info">
+            <a href="#" className="link-1">Explore The Library</a>
+            <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+            <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+            <a href="#" className="link-2">Request Early Access to Get Started</a>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
