3baf4a4e94ef21b61cb34981c5e4f10890b30e4d
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-06-23 20:13:37 +0600

    CTA Component Markup, Style and Responsiveness has done

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import "./CTA.css";
 
 const CTA = () => {
   return (
-    <div>
-      <h3>CTA</h3>
-    </div>
+    <section className="gpt3__CTA">
+      <div className="container">
+        <div className="gpt3__CTA-content">
+          <div className="gpt3__CTA-content-info">
+            <div className="gpt3__CTA-info_text">
+              <a href="#">Request Early Access to Get Started</a>
+              <h1>Register today & start exploring the endless possibilities.</h1>
+            </div>
+            <button type="button">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
