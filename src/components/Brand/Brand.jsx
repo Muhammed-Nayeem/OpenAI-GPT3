43208d2a3b5b44441f26508503b7c65e5cae6fed
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-06-23 11:42:49 +0600

    Brand Component Done

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import "./Brand.css";
+import { google, slack, atlassian, dropBox, shopify } from './importImages';
 
 const Brand = () => {
   return (
-    <div>
-      <h3>Brand</h3>
-    </div>
+    <section className="gpt3__brand">
+      <div className="container">
+        <div className="gpt3__brand-content">
+          <div className="gpt3__brand-img">
+            <img src={google} alt="Google" />
+          </div>
+          <div className="gpt3__brand-img">
+            <img src={slack} alt="Slack" />
+          </div>
+          <div className="gpt3__brand-img">
+            <img src={atlassian} alt="Atlassian" />
+          </div>
+          <div className="gpt3__brand-img">
+            <img src={dropBox} alt="DropBox" />
+          </div>
+          <div className="gpt3__brand-img">
+            <img src={shopify} alt="Shopify" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
