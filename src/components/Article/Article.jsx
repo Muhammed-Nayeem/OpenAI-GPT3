d93f8355044fc53d9193180b5d585bd17556719f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-06-24 14:17:28 +0600

    Blog and Article component markup, styles and responsiveness has done

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Article.css";
 
-const Article = () => {
+const Article = ({ imageUrl, date, title }) => {
   return (
-    <div>
-      <h3>Article</h3>
-    </div>
+    <article className="gpt3__blog-container_article">
+      <div className="gpt3__blog-img">
+        <img src={imageUrl} alt="Blog-Image" />
+      </div>
+      <div className="gpt3__blog-content">
+        <div className="gpt3__blog-content_text">
+          <span>{date}</span>
+          <h3>{title}</h3>
+        </div>
+        <a href="#">Read Full Details</a>
+      </div>
+    </article>
   );
 };
 
--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React from "react";
+import "./Blog.css";
+import { blogImage1, blogImage2, blogImage3, blogImage4, blogImage5 } from "./importImages";
+import { Article } from "../../components";
 
 const Blog = () => {
   return (
-    <div>
-      <h3>Blog</h3>
-    </div>
+    <section className="gpt3__blog-area">
+      <div className="container">
+        <article className="gpt3__blog-title">
+          <h1 className="gradient__text">
+            A lot is happening,
+            <br />
+            We are blogging about it.
+          </h1>
+        </article>
+        <div className="gpt3__blog-container">
+          <div className="gtp3__blog-container_group-A">
+            <Article
+              imageUrl={blogImage1}
+              date="Sep 26, 2021"
+              title="GPT-3 and Open  AI is the future. Let us explore how it is?"
+            />
+          </div>
+          <div className="gpt3__blog-container_group-B">
+            <Article
+              imageUrl={blogImage2}
+              date="June 18, 2022"
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            />
+            <Article
+              imageUrl={blogImage3}
+              date="June 15, 2023"
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            />
+            <Article
+              imageUrl={blogImage4}
+              date="June 19, 2023"
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            />
+            <Article
+              imageUrl={blogImage5}
+              date="June 24, 2023"
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
